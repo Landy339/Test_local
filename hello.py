01b5892ca7427aee377fceef3a29bb1b9a2598bc
Landy339 <huangxin199074@hotmail.com>
2022-06-05 20:33:22 +1000

    added myname module

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  3 15:46:21 2022
-
-@author: Valued Customer
-"""
-
 # hello.py
-print('hello Git!')
+import myname
+
+name = myname.get_name()
+print("hello {}".format(name))
